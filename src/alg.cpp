4f448ea37a2121c0136ea1e5d2ecda775850bcea
Nikita-Erem <160235350+Nikita-Erem@users.noreply.github.com>
2024-03-04 20:39:26 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,19 @@
-// Copyright 2021 NNTU-CS
+//"Copyright 2024 Microsoft Corporation"
+#include <iostream>
+#include "alg.h"
 
+int gcd(int, int);
+int main() {
+    std::cout << gcd(10, 15) << std::endl;
+    return 0;
+}
 int gcd(int a, int b) {
-    // поместить сюда текст реализации алгоритма (на С++)
+    int max = (a > b) ? a : b;
+    int y = 0;
+    for (int i = 1; i < max; i++) {
+        if ((a % i == 0) && (b % i == 0)) {
+            y = i;
+        }
+    }
+    return y;
 }
