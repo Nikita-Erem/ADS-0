e1d4393663ef6c890d2d70cf7f3ec3e7dd470c29
Nikita-Erem <160235350+Nikita-Erem@users.noreply.github.com>
2024-03-04 20:57:51 +0300

    1 alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,19 +1,46 @@
-//"Copyright 2024 Microsoft Corporation"
+// Copyright 2022 NNTU-CS
+#include <cstdint>
 #include <iostream>
 #include "alg.h"
 
-int gcd(int, int);
-int main() {
-    std::cout << gcd(10, 15) << std::endl;
-    return 0;
-}
-int gcd(int a, int b) {
-    int max = (a > b) ? a : b;
-    int y = 0;
-    for (int i = 1; i < max; i++) {
-        if ((a % i == 0) && (b % i == 0)) {
-            y = i;
+
+bool checkPrime(uint64_t value) {
+for (uint64_t j = 2; j < value; j++) {
+        if (value % j == 0) {
+            return false;
         }
     }
-    return y;
+    return true;
+}
+}
+
+uint64_t nPrime(uint64_t n) {
+    int ccc = 0;
+    int number = 2;
+    while (ccc < n) {
+        if (isPrime(number)) {
+            ccc++;
+        }
+        number++;
+    }
+    return number - 1;
+}
+
+uint64_t nextPrime(uint64_t value) {
+    if (value <= 1) return 0;
+    value++;
+    while (!checkPrime(value)) {
+        value++;
+    }
+    return value;
+}
+
+uint64_t sumPrime(uint64_t hbound) {
+    uint64_t summ = 0;
+    for (uint64_t i = 2; i < hbound; i++) {
+        if (checkPrime(i)) {
+            summ += i;
+        }
+    }
+    return summ;
 }
